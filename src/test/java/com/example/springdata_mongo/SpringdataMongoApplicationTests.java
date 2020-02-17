72ef1dad8bfbe782455330648b1de02110b75d01
k.ambroziak <k.ambroziak@samsung.com>
2020-02-17 17:35:04 +0100

    springdata standard

---
+++ src/test/java/com/example/springdata_mongo/SpringdataMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdata_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
